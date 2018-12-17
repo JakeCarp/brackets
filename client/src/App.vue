3478acdd7cbc088c00adb4637ca4b54d8120f9fe
Joshua Thurston <joshthurston92@gmail.com>
2018-12-17 14:37:05 -0700

    navbar changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">Brackets</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
         aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -12,11 +12,9 @@
           <!-- <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li> -->
-          <!-- <router-link class="home" to="/">Home</router-link>
-          <router-link to="/login">Login</router-link> -->
-          <li class="nav-item">
-            <a class="nav-link" @click="router.push(login.vue)  " href="#">Home</a>
-          </li>
+          <router-link class="home" to="/">Home</router-link>
+          <router-link class="home" to="/login">Login</router-link>
+
           <li class="nav-item">
             <a class="nav-link" href="#">About</a>
           </li>
@@ -83,6 +81,10 @@
   #nav a {
     font-weight: bold;
     color: #2c3e50;
+  }
+
+  .home {
+    color: rgba(0, 0, 0, 0.5);
   }
 
   .navbar {
