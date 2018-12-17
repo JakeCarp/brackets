acc08cab010809ce79e21bc90f73299232db024b
Joshua Thurston <joshthurston92@gmail.com>
2018-12-17 14:24:12 -0700

    active and nav bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,68 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0 menu">
+          <!-- <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li> -->
+          <!-- <router-link class="home" to="/">Home</router-link>
+          <router-link to="/login">Login</router-link> -->
+          <li class="nav-item">
+            <a class="nav-link" @click="router.push(login.vue)  " href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Create</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Join</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/login">Login</router-link> |
       <router-link to="/"><i class="fas fa-bars"></i></router-link>
       <div class="pos-f-t">
         <div class="collapse" id="navbarToggleExternalContent">
           <div class="bg-dark p-4">
-            <h5 class="text-white h4">Collapsed content</h5>
-            <span class="text-muted">Toggleable via the navbar brand.</span>
+            <h5>
+              <router-link to="/">Home</router-link>
+            </h5>
+            <h5>
+              <router-link to="/login">Login</router-link>
+            </h5>
+            <h5 class="text-white h4">About</h5>
+            <h5 class="text-white h4">Create</h5>
+            <h5 class="text-white h4">Join</h5>
+            <h5 class="text-white h4">Active</h5>
+            <h5 class="text-white h4">Logout</h5>
           </div>
         </div>
-        <nav class="navbar navbar-dark bg-dark">
+        <nav class="navbar navbar-dark bg-dark d-flex justify-content-end">
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent"
             aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
         </nav>
       </div>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
@@ -41,7 +85,7 @@
     color: #2c3e50;
   }
 
-  #nav a.router-link-exact-active {
-    color: #42b983;
+  .navbar {
+    margin-bottom: 2rem;
   }
 </style>
---
+++ client/src/views/active.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="active container-fluid">
+    <activeComponent class="row"></activeComponent>
+  </div>
+</template>
+
+<script>
+  import activeComponent from "@/components/activeComponent"
+
+  export default {
+    name: 'active',
+    data() {
+      return {
+
+      }
+    },
+    computed: {},
+    methods: {},
+    components: {
+      activeComponent
+    },
+    props: [],
+  }
+
+</script>
+
+<style>
+  .card-header {
+    width: 100vw;
+  }
+</style>
