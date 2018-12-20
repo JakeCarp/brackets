512010b43ca99b3d4ab45cd3e3ff60fc7217e734
Joshua Thurston <joshthurston92@gmail.com>
2018-12-20 10:12:13 -0700

    working on generating

--- client/src/store.js
+++ client/src/store.js
@@ -24,7 +24,9 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     user: {},
-    tournament: {},
+    tournament: {
+      entries: ["player 1", "player 2", "player 3", "player 4", "player 5", "player 6", "player 7", "player 8", "player 9", "player 10", "player 11", "player 12", "player 13", "player 14", "player 15", "player 16", "player 17"]
+    },
   },
   mutations: {
     setUser(state, user) {
@@ -66,6 +68,14 @@ export default new Vuex.Store({
     },
 
     //tournaments
+    getTournaments({ commit, dispatch }) {
+
+      api.get('tournament/')
+        .then(res => {
+          commit('setTournament', res.data)
+        })
+    },
+
     getTournament({ commit, dispatch }, tournamentId) {
 
       api.get('tournament/' + tournamentId)
@@ -107,7 +117,97 @@ export default new Vuex.Store({
         .then(tournament => {
           dispatch('getTournament')
         })
+    },
+
+
+
+
+
+    //testing tourney generation
+    //finding the sweetSpot 
+    calcPreGames({ commit, dispatch }, payload) {
+      debugger
+      for (let i = 0; i < payload.sweetSpots.length; i++) {
+        if (payload.sweetSpots[i] > payload.entries) {
+          return payload.sweetSpots[i - 1]
+        }
+      }
+    },
+
+
+    //making the tree
+    buildTree({ commit, dispatch }, arr) {
+      let root = { text: { name: "winner" } }
+      let preGamesNeeded = arr.length - dispatch('calcPreGames'(sweetSpots, arr.length))
+      let tree = []
+
+      let competitors = arr.length - preGamesNeeded
+      let i = 1
+      let buyInstance = 0
+      while (competitors > 1) {
+        let round = []
+        tree.push(buildRound(competitors, round, i))
+        competitors = round.length / 2
+        i++
+      }
+
+      function assignParents() {
+        tree.push([root])
+        tree.reverse()
+        console.log(tree)
+        debugger
+        let nextRound = []
+        for (let j = tree.length - 1; j > 0; j--) {
+          const currRound = tree[j];
+          nextRound = tree[j - 1]
+
+          let pi = 0
+          for (let z = 0; z < currRound.length; z += 2) {
+            let currentP = nextRound[pi]
+            const entry1 = currRound[z];
+            const entry2 = currRound[z + 1];
+            entry1.parent = currentP
+            if (entry2) {
+              entry2.parent = currentP
+            }
+            pi++
+          }
+        }
+      }
+      assignParents()
+
+      function assignPreGames() {
+        let preGames = []
+        for (let pg = 1; pg <= preGamesNeeded; pg++) {
+          preGames.push({ text: { name: "pregame " + pg } }, { text: { name: "pregame " + pg } })
+        }
+        let parentCount = 0
+        for (let i = 0; i < preGames.length; i += 2) {
+          preGames[i].parent = tree[tree.length - 1][parentCount]
+          preGames[i + 1].parent = tree[tree.length - 1][parentCount]
+          parentCount++
+        }
+        tree.push(preGames)
+      }
+      assignPreGames()
+
+      function buildRound(competitors, round, roundNum) {
+        for (var i = 0; i < competitors; i++) {
+          round.push({ text: { name: "match " + (i + 1) + ' round ' + roundNum } })
+        }
+        return round
+      }
+
+      let bracketArray = [].concat(...tree)
+
+      for (let b = 0; b < arr.length; b++) {
+        const person = arr[b];
+        bracketArray[(bracketArray.length - 1) - b].text.name = person
+      }
+      return { tree, bracketArray }
     }
+
+
+    // router.push({ name: 'join', params: { entryCode: entryCode } })
   }
 })
-// router.push({ name: 'join', params: { entryCode: entryCode } })
