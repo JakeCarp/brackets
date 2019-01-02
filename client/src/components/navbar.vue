bb845a3cc0f99cfc7f5f53a8173b0e73bbf690e6
Joel Schwartz <nederlands18@gmail.com>
2019-01-02 11:01:41 -0700

    push

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -16,7 +16,7 @@
             <router-link class="home font" data-toggle="collapse" data-target="#navbarToggleExternalContent" to="/about">About</router-link>
             <router-link class="home font" data-toggle="collapse" data-target="#navbarToggleExternalContent" to="/create">Create</router-link>
             <router-link class="home font" data-toggle="collapse" data-target="#navbarToggleExternalContent" :to="{name: 'join'}">Join</router-link>
-            <router-link class="home font" data-toggle="collapse" data-target="#navbarToggleExternalContent" to="/active">Active</router-link>
+            <!-- <router-link class="home font" data-toggle="collapse" data-target="#navbarToggleExternalContent" to="/active">Active</router-link> -->
             <router-link class="home font" data-toggle="collapse" data-target="#navbarToggleExternalContent" to="/test">Test
               Bracket</router-link>
             <router-link class="home font" data-toggle="collapse" data-target="#navbarToggleExternalContent" to="/bracket">
