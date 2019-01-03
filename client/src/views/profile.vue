041b8708f99f98ddce08b8f99f2823574bfb9c1b
Joshua Thurston <joshthurston92@gmail.com>
2019-01-03 10:50:28 -0700

    upload

--- client/src/views/profile.vue
+++ client/src/views/profile.vue
@@ -3,13 +3,6 @@
     <navbar />
     <div class="profile container-fluid">
       <profileComponent></profileComponent>
-<<<<<<< HEAD
-=======
-      <form>
-        <input type="text-field" name="userBio">
-        <button type="submit">Add Bio</button>
-      </form>
->>>>>>> d2dc9be9cf499df52c969a6e0b713ed3635f1c4f
     </div>
   </div>
 </template>
