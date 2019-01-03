92cda73a437ea0e5a37490afc0caaf5e97e40052
rioOropeza <blasphemy678@live.com>
2019-01-03 10:37:05 -0700

    entry components

---
+++ client/src/components/singleEntry.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="single-entry">
+    <!-- single entry -->
+    <form @submit.prevent="createEntry" v-if="$route.params.tournamentId">
+      <h1>Single Entry</h1>
+      <input placeholder="Add Player" type="text" v-model="newEntry.name">
+      <button type="submit">Submit</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'single',
+    data() {
+      return {
+        newEntry: {
+          name: "",
+          tournamentId: this.$route.params.tournamentId,
+          members: []
+        }
+      }
+    },
+    computed: {},
+    methods: {
+      createEntry() {
+        this.$store.dispatch('createEntry', this.newEntry)
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+
+</style>
---
+++ client/src/components/teamEntry.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="team-entry">
+    <!-- team entry -->
+    <form @submit.prevent="createEntry" v-if="$route.params.tournamentId">
+      <h1>Team Entry</h1>
+      <input placeholder="Add Player" type="text" v-model="playerName">
+      <input placeholder="Team Name" type="text" v-model="newEntry.name">
+      <button type="submit">Submit</button>
+      <div v-if="playerName.length > 0">
+        <div v-for="player in playersToAdd">
+          <p @click="addPlayer(player)">{{player.name}}</p>
+        </div>
+      </div>
+      <h5>Members</h5>
+      <p v-for="member in newEntry.members">{{member.name}}</p>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'team',
+    data() {
+      return {
+        newEntry: {
+          name: "",
+          tournamentId: this.$route.params.tournamentId,
+          members: []
+        },
+        playerName: ""
+      }
+    },
+    computed: {
+      profiles() {
+        return this.$store.state.profiles
+      },
+      playersToAdd() {
+        var keyword = this.playerName
+        var temp = this.profiles
+        var players = []
+        for (var i = 0; i < temp.length; i++) {
+          var player = temp[i]
+          var value = player.name.toLowerCase()
+          var term = keyword.toLowerCase()
+          if (value.indexOf(term) !== -1 && players.length < 20) {
+            players.push(player)
+          }
+        }
+        return players
+      }
+    },
+    methods: {
+      addPlayer(player) {
+        this.newEntry.members.push(player)
+        this.playerName = ""
+      },
+      createEntry() {
+        this.$store.dispatch('createEntry', this.newEntry)
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+
+</style>
