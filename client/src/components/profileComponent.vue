157d20631042b3bb66221c31308e5d483e657477
Joshua Thurston <joshthurston92@gmail.com>
2019-01-03 17:00:39 -0700

    modals

--- client/src/components/profileComponent.vue
+++ client/src/components/profileComponent.vue
@@ -5,13 +5,61 @@
       <div class="col-3">
         <div class="row">
           <div class="col-12"> <img :src="getUser.picture" alt=""></div>
-          <div class="col-12">
-            <form>
+          <div class="col-6 picBtn">
+            <!-- modal start -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#changePic">
+              Change Picture </button>
+            <div class="modal" id="changePic" tabindex="-1" role="dialog">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title">Enter Image URL</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <input type="text-field" style="width:350px">
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- modal end -->
+          </div>
+          <div class="col-6 bioBtn">
+            <!-- modal start -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#changeBio">
+              Edit Bio </button>
+            <div class="modal" id="changeBio" tabindex="-1" role="dialog">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title">Edit Bio</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <input type="text-field" style="width:350px">
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- modal end -->
+          <!-- <form>
               <button type="button" class="btn btn-outline-primary mt-1" @click="changePicture">Change
                 Picture</button>
-            </form>
-          </div>
-          <div class="col-12">
+            </form> -->
+          <!-- <div class="col-12">
             <form v-if="!getUser.bio">
               <input type="text-field">
               <button type="submit">Add Bio</button>
@@ -21,7 +69,7 @@
               <button type="submit">Change Bio</button>
             </form>
             {{getUser.bio}}
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="col-9">
@@ -99,5 +147,15 @@
     /* display: flex; */
     /* justify-content: space-between; */
     align-items: center;
+  }
+
+  .picBtn {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .bioBtn {
+    display: flex;
+    justify-content: flex-start;
   }
 </style>
