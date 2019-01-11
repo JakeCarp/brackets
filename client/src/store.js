9eacb0e3c9a2f4023604d77cb5c55dd8caa3590a
Jake Carpenter <jakecarpenter407@gmail.com>
2019-01-11 16:35:36 -0700

    chat socket added

--- client/src/store.js
+++ client/src/store.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Axios from 'axios'
 import router from './router'
+import io from 'socket.io-client'
+let socket = {}
 
 let baseUrl = '//localhost:3000/'
 
@@ -23,6 +25,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    chatJoined: false,
+    chatMessages: [],
+    roomData: {},
+    chatName: {},
     profiles: [],
     user: {},
     schedule: {},
@@ -36,10 +42,40 @@ export default new Vuex.Store({
     bracketArray: []
   },
   mutations: {
+    //tournament chat mutations
+    //#region 
+    setJoined(state, payload) {
+      state.chatJoined = true
+      state.chatName = payload
+    },
+    setRoom(state, payload) {
+      state.roomData = payload;
+    },
+    newChatUser(state, payload) {
+      Vue.set(state.roomData.connectedUsers, payload.userName, payload.userName)
+    },
+    userLeft(state, payload) {
+      Vue.set(state.roomData.connectedUsers, payload, undefined)
+    },
+    addMessage(state, payload) {
+      state.chatMessages.push(payload)
+    },
+    leave(state) {
+      state.chatJoined = false
+      state.chatName = ''
+      state.chatMessages = []
+      state.roomData = {}
+    },
+    //#endregion
+    //Auth mutations
+    //#region 
     setUser(state, user) {
       state.user = user
       console.log(user)
     },
+    //#endregion
+    //set tournament mutations
+    //#region 
     setTournament(state, tournament) {
       state.tournament = tournament
     },
@@ -49,10 +85,16 @@ export default new Vuex.Store({
     setTournaments2(state, tournaments) {
       state.tournaments = tournaments
     },
+    //#endregion
+    //Entry mutations
+    //#region 
     setEntry(state, entry) {
       state.entry = entry
       console.log(entry)
     },
+    //#endregion
+    //misc mutations
+    //#region 
     setProfiles(state, profiles) {
       state.profiles = profiles
     },
@@ -64,6 +106,7 @@ export default new Vuex.Store({
     }
 
   },
+  //#endregion
   actions: {
     //Tournament chat actions
     //#region 
@@ -79,7 +122,7 @@ export default new Vuex.Store({
       socket.on('CONNECTED', data => {
         console.log('Connected to socket')
         //connect to room
-        socket.emit('join', {name:payload})
+        socket.emit('join', { name: payload })
       })
       socket.on('joinedRoom', data => {
         commit('setRoom', data)
@@ -95,7 +138,14 @@ export default new Vuex.Store({
         commit('addMessage', data)
       })
     },
-    sendMessage
+    sendMessage({ commit, dispatch }, payload) {
+      socket.emit('message', payload)
+    },
+    leaveRoom({ commit, dispatch }, payload) {
+      socket.emit('leave')
+      socket.close()
+      commit('leave')
+    },
     //#endregion
     //auth actions
     //#region 
@@ -128,8 +178,9 @@ export default new Vuex.Store({
         })
       router.push({ name: 'login' })
     },
-
-    //tournaments
+    //#endregion
+    //tournaments actions
+    //#region 
     getTournaments({ commit, dispatch }) {
       api.get('tournament/')
         .then(res => {
@@ -220,6 +271,9 @@ export default new Vuex.Store({
           commit('setEntry', res.data)
         })
     },
+    //#endregion
+    //misc actions
+    //#region 
     getAllProfiles({ commit, dispatch }) {
       api.get('entry/')
         .then(res => {
@@ -232,10 +286,12 @@ export default new Vuex.Store({
           commit("setSchedule", res.data)
         })
     },
+    //#endregion
 
 
 
-
+    //Treant actions
+    //#region 
 
     //testing tourney generation
     //finding the sweetSpot 
@@ -427,3 +483,4 @@ export default new Vuex.Store({
 
 // //     // router.push({ name: 'join', params: { entryCode: entryCode } })
 // // )}
+//#endregion
--- server/index.js
+++ server/index.js
@@ -3,22 +3,13 @@ var bp = require('body-parser')
 var server = express()
 var cors = require('cors')
 var port = 3000
-// var socketServer = new (require('http').Server)(server);
-// var io = require('socket.io')(socketServer);
 
-// socketServer.listen(80)
+//create server for socked and connect socket to it
+var socketServer = require('http').createServer(server);
+var io = require("socket.io")(socketServer);
 
-// server.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// });
-// //socket implementation
-// io.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('YEEET!', function (data) {
-//     console.log(data)
-//   })
-// })
-
+let Socket = require("./socket")
+let socket = new Socket(io)
 
 var whitelist = ['http://localhost:8080'];
 var corsOptions = {
